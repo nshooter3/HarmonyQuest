6b933471bc512bf8ff465be1d206a5b60eb52971
nshooter3 <nick.shooter@gmail.com>
2019-05-16 02:40:31 -0400

    first pass at character controller. reverted project settings and package manager to older version of unity

---
+++ Assets/NickZone/Scripts/TestPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed;
+    public float gravity;
+    public float rotateSpeed;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 moveDirectionNoGravity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+        }
+
+        moveDirectionNoGravity = moveDirection;
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        //Rotate player to face movement direction
+        if (moveDirectionNoGravity.magnitude > 0)
+        {
+            Vector3 targetPos = transform.position + moveDirectionNoGravity;
+            Vector3 targetDir = targetPos - transform.position;
+
+            // The step size is equal to speed times frame time.
+            float step = rotateSpeed * Time.deltaTime;
+
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+
+            // Move our position a step closer to the target.
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+        //Failsafe to ensure that x and z are always zero.
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+    }
+}
