7cf99df9267cd0180f8ac1a457585860c3118fe7
nshooter3 <nick.shooter@gmail.com>
2019-06-01 01:34:44 -0400

    Enemy now chases player

--- Assets/NickZone/Scripts/TestEnemy.cs
+++ Assets/NickZone/Scripts/TestEnemy.cs
@@ -8,6 +8,16 @@ public class TestEnemy : MonoBehaviour
     public int health;
     public int attackDamage = 10;
 
+    public float speed;
+    public float gravity;
+    public float rotateSpeed;
+
+    public bool pursuePlayer = true;
+
+    [SerializeField]
+    private CharacterController characterController;
+    [SerializeField]
+    private TestPlayer player;
     [SerializeField]
     private GameObject attackBox;
     [SerializeField]
@@ -38,6 +48,9 @@ public class TestEnemy : MonoBehaviour
 
     float beatCount = 0;
 
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 moveDirectionNoGravity = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -64,6 +77,11 @@ public class TestEnemy : MonoBehaviour
 
         beatCount += Time.deltaTime;
 
+        if (pursuePlayer)
+        {
+            PursuePlayer();
+        }
+
         /*(if (WasAttackedOnBeat())
         {
             enemyMat.color = windUpColor;
@@ -84,6 +102,44 @@ public class TestEnemy : MonoBehaviour
                 EndAttack();
             }
         }
+    }
+
+    void PursuePlayer()
+    {
+        moveDirection = (player.transform.position - transform.position).normalized * speed;
+
+        moveDirectionNoGravity = moveDirection;
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        RotateEnemy(1.0f);
+    }
+
+    void RotateEnemy(float turnSpeedModifier)
+    {
+        //Rotate player to face movement direction
+        if (moveDirectionNoGravity.magnitude > 0)
+        {
+            Vector3 targetPos = transform.position + moveDirectionNoGravity;
+            Vector3 targetDir = targetPos - transform.position;
+
+            // The step size is equal to speed times frame time.
+            float step = rotateSpeed * turnSpeedModifier * Time.deltaTime;
+
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+
+            // Move our position a step closer to the target.
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
+        //Failsafe to ensure that x and z are always zero.
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
     }
 
     public bool IsAttacking()
@@ -108,8 +164,8 @@ public class TestEnemy : MonoBehaviour
         }
         else
         {
-            metronomeSound.pitch = 0.7f;
-            metronomeSound.Play();
+            //metronomeSound.pitch = 0.7f;
+            //metronomeSound.Play();
         }
 
         //Giant switch statements, the mark of a true prototype. We gotta come up with a more eloquent way to do beat based actions.
