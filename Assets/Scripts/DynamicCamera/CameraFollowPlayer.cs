6f3bc959c588f99433d168ad07e831f2cef8e887
Mitch McClellan <mitchelljmcclellan@gmail.com>
2019-08-08 00:54:10 -0400

    Tweaked Camera Follow Player script, replacing magic numbers with variables

--- Assets/Scripts/DynamicCamera/CameraFollowPlayer.cs
+++ Assets/Scripts/DynamicCamera/CameraFollowPlayer.cs
@@ -11,9 +11,12 @@
         public Vector3 lowAngle;
         public Vector3 highAngle;
         [SerializeField]
-        private Vector3 cameraOffset;
-        [SerializeField]
         protected float followBias;
+
+        private float exponentialHeight;
+        private float exponentFactor = 0.5f;
+        private float exponentBase = 1.5f;
+        private float cameraOffset = 3.2f;
 
         void Awake()
         {
@@ -23,12 +26,12 @@
         void Update()
         {
             bias = followBias;
-            float exponentialHeight = Mathf.Pow(1.5f, distance / 2.0f) + minHeight;
+            // Camera Height calculated on an exponential curve
+            exponentialHeight = Mathf.Pow(exponentBase, distance * exponentFactor) + minHeight;
             direction = (transform.position - PlayerLocation()).normalized * distance;
             direction.x = 0;
             direction.y = exponentialHeight;
-            direction.z -= 1.6f;
-            exponentialHeight = Mathf.Pow(1.5f, distance/1.5f) + minHeight;
+            direction.z -= cameraOffset;
             targetAngles = Vector3.Lerp(lowAngle, highAngle, exponentialHeight / maxHeight);
             direction += PlayerLocation() + PlayerVelocity() / Mathf.Lerp(5f, 1.2f, transform.position.y / maxHeight);
             Camera.main.fieldOfView = Mathf.Lerp(73f, 65f, exponentialHeight / maxHeight);
