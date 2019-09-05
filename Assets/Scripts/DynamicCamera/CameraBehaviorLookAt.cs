3558806837c4166aea61ac54a0c3110d18f5b248
Mitch McClellan <mitchelljmcclellan@gmail.com>
2019-09-04 22:48:07 -0400

    Setup Camera System (#2)
    * Adding first iteration of smooth camera and the camera test scene
    
    * Set up dynamic camera system
    
    * Finished replicating the follow player and point of interest behaviors with the new camera
    
    * Reworked the GetCamera function to actually use generics and moved common functions to the CameraBehavior base class
    
    * Added a closer follow player angle and curved transition between the two positions
    
    * Tweaked Camera Follow Player script, replacing magic numbers with variables
    
    * Cleaned up extra commented out code and moved debug art
    
    * Reset NickZone Camera and Player scripts
    
    * Missed a new line character in TestCamera
    
    * Incorporated feedback from Camera Setup pull request.
    Removed vestigial variables and functions, added singleton check in the CameraController Awake, set the behaviors list to all the behaviors currently attached to the camera, changed the toggle camera to activate the first camera in the list, removed more magic numbers in FollowPlayer
    
    * Restructured the CameraBehaviors to remove their monobehavior inheritance
    
    * Updated CameraTest scene
    
    * Changed the camera behavior init to take in a reference to the player instead of finding it itself
    
    * Renamed CameraBehavior scripts to be more clear

---
+++ Assets/Scripts/DynamicCamera/CameraBehaviorLookAt.cs
@@ -0,0 +1,29 @@
+﻿namespace HarmonyQuest.DynamicCamera
+{
+    using UnityEngine;
+
+    public class CameraBehaviorLookAt : CameraBehavior
+    {
+        public Transform targetPoint;
+        private Vector3 distanceFromPlayer = new Vector3(0, 9.5f, -10);
+
+        public override void Init(Transform cameraTransform, TestPlayer player)
+        {
+            base.Init(cameraTransform, player);
+            bias = 1.5f;
+            targetAngles = new Vector3(45, 0, 0);
+        }
+
+        public override void Update()
+        {
+            if (targetPoint != null)
+            {
+                direction = Vector3.Lerp(PlayerLocation(), targetPoint.position, 0.5f) + distanceFromPlayer;
+            }
+            else
+            {
+                direction = Vector3.zero;
+            }
+        }
+    }
+}
