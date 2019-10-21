c3da402629eda1e3e4c82b89abd66af86b02292e
nshooter3 <nick.shooter@gmail.com>
2019-10-20 22:58:06 -0400

    first pass at basic pathfinding

---
+++ Assets/Scripts/AI/NavMeshTraversal.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class NavMeshTraversal : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (agent == null)
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+        agent.isStopped = true;
+
+        if (target != null)
+        {
+            SetTarget(target);
+            StartNavigation();
+        }
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            SetTarget(target);
+        }
+    }
+
+    public void SetTarget(Transform target)
+    {
+        agent.destination = target.position;
+    }
+
+    public void StartNavigation()
+    {
+        agent.isStopped = false;
+    }
+
+    public void StopNavigation()
+    {
+        agent.isStopped = true;
+    }
+
+    public void SetStoppingDistance(float stoppingDistance)
+    {
+        agent.stoppingDistance = stoppingDistance;
+    }
+
+    public float GetDistanceFromTarget()
+    {
+        return agent.remainingDistance;
+    }
+}
