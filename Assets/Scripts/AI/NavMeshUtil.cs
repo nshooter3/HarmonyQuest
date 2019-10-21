2e576212e6b620ebade5779c4fcad774422cc871
nshooter3 <nick.shooter@gmail.com>
2019-10-21 01:02:46 -0400

    Undid sandbox folder, as that should be done on a different branch. Changed the navmash script to a static util script, in order to decouple our agent traversal from the built in unity pathfinding.

---
+++ Assets/Scripts/AI/NavMeshUtil.cs
@@ -0,0 +1,37 @@
+﻿namespace AI
+{
+    using UnityEngine.AI;
+    using UnityEngine;
+
+    public static class NavMeshUtil
+    {
+        public static NavMeshPath GeneratePath(Transform source, Transform target, int areaMask = NavMesh.AllAreas)
+        {
+            NavMeshPath path = new NavMeshPath();
+            NavMesh.CalculatePath(source.position, target.transform.position, areaMask, path);
+            return path;
+        }
+
+        public static float GetPathLength(NavMeshPath path)
+        {
+            if (path.status == NavMeshPathStatus.PathInvalid)
+            {
+                return -1;
+            }
+
+            float length = 0.0f;
+            for (int i = 1; i < path.corners.Length; ++i)
+            {
+                length += Vector3.Distance(path.corners[i - 1], path.corners[i]);
+            }
+
+            return length;
+        }
+
+        public static bool IsTargetUnobstructed(Transform source, Transform target, int areaMask = NavMesh.AllAreas)
+        {
+            NavMeshHit hit;
+            return NavMesh.Raycast(source.position, target.position, out hit, areaMask);
+        }
+    }
+}
