531a6c9c6a52d2463894765e460b148fb65abb0d
nshooter3 <nick.shooter@gmail.com>
2019-10-21 03:53:15 -0400

    first pass at custom navigation agent and generic enemy

---
+++ Assets/Scripts/AI/BasicNavMeshEnemy.cs
@@ -0,0 +1,86 @@
+﻿namespace AI
+{
+    using UnityEngine;
+
+    public class BasicNavMeshEnemy : NavMeshTraveler
+    {
+        public Transform aggroTarget;
+        public Transform bottom;
+
+        public GameObject navPos;
+
+        public float speed;
+        public float gravity;
+        public float rotateSpeed;
+
+        public bool showDestination = false;
+
+        [SerializeField]
+        private Rigidbody rb;
+
+        private Vector3 moveDirection = Vector3.zero;
+        private Vector3 moveDirectionNoGravity = Vector3.zero;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetTarget(bottom, aggroTarget);
+            if (rb == null)
+            {
+                rb = GetComponent<Rigidbody>();
+            }
+            navPos.transform.parent = null;
+            navPos.SetActive(showDestination);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            NavMeshTravelerUpdate();
+            Move();
+        }
+
+        void Move()
+        {
+            Vector3 destination = GetNextWaypoint();
+            navPos.transform.position = destination;
+            //print("DESTINATION = " + destination);
+            //print("BOTTOM POS = " + bottom.position);
+            moveDirection = (destination - bottom.position).normalized;
+
+            moveDirectionNoGravity = moveDirection;
+
+            // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+            // as an acceleration (ms^-2)
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            // Move the controller
+            rb.velocity = (moveDirection * Time.deltaTime) * speed;
+            //print("RB VELOCITY: " + rb.velocity);
+
+            RotateEnemy(1.0f);
+        }
+
+        void RotateEnemy(float turnSpeedModifier)
+        {
+            //Rotate player to face movement direction
+            if (moveDirectionNoGravity.magnitude > 0)
+            {
+                Vector3 targetPos = transform.position + moveDirectionNoGravity;
+                Vector3 targetDir = targetPos - transform.position;
+
+                // The step size is equal to speed times frame time.
+                float step = rotateSpeed * turnSpeedModifier * Time.deltaTime;
+
+                Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+                Debug.DrawRay(transform.position, newDir, Color.red);
+
+                // Move our position a step closer to the target.
+                transform.rotation = Quaternion.LookRotation(newDir);
+            }
+            //Failsafe to ensure that x and z are always zero.
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/AI/NavMeshTraveler.cs
@@ -0,0 +1,95 @@
+﻿namespace AI
+{
+    using System.Collections.Generic;
+    using UnityEngine.AI;
+    using UnityEngine;
+
+    public class NavMeshTraveler : MonoBehaviour
+    {
+        [HideInInspector]
+        public bool isTraversalActive = false;
+        
+        /// <summary>
+        /// How frequently we check to see if our path to our target should change.
+        /// </summary>
+        [SerializeField]
+        private float pathRefreshRate = 0.5f;
+
+        /// <summary>
+        /// How far our target must move from its last known position to warrant generating a new path.
+        /// </summary>
+        [SerializeField]
+        private float pathRefreshDistanceThreshold = 2.0f;
+
+        /// <summary>
+        /// How close we need to be to a waypoint for it to be considered reached.
+        /// </summary>
+        [SerializeField]
+        private float waypointReachedDistanceThreshold = 2.0f;
+
+        private Transform source;
+        private Transform target;
+        private Vector3 lastKnownTargetPos = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+
+        private NavMeshPath path;
+        private Queue<Vector3> waypoints;
+        private Vector3 nextWaypoint;
+        private float pathRefreshTimer = 0;
+
+        public void SetTarget(Transform source, Transform target)
+        {
+            this.source = source;
+            this.target = target;
+            isTraversalActive = true;
+            GeneratePathToTarget();
+        }
+
+        public Vector3 GetNextWaypoint()
+        {
+            return nextWaypoint;
+        }
+
+        public void NavMeshTravelerUpdate()
+        {
+            if (isTraversalActive == true && target != null)
+            {
+                CheckIfPathNeedsToBeRegenerated();
+                UpdateDestination();
+            }
+        }
+
+        void CheckIfPathNeedsToBeRegenerated()
+        {
+            pathRefreshTimer += Time.deltaTime;
+            if (pathRefreshTimer > pathRefreshRate)
+            {
+                pathRefreshTimer = 0;
+                if (Vector3.Distance(target.transform.position, lastKnownTargetPos) > pathRefreshDistanceThreshold)
+                {
+                    GeneratePathToTarget();
+                }
+            }
+        }
+
+        void UpdateDestination()
+        {
+            if (Vector3.Distance(source.position, nextWaypoint) <= waypointReachedDistanceThreshold)
+            {
+                if (waypoints.Count > 0)
+                {
+                    nextWaypoint = waypoints.Dequeue();
+                    print("GENERATE NEW WAYPOINT: " + nextWaypoint);
+                }
+            }
+        }
+
+        void GeneratePathToTarget()
+        {
+            print("GeneratePathToTarget");
+            path = NavMeshUtil.GeneratePath(source, target);
+            waypoints = new Queue<Vector3>(path.corners);
+            nextWaypoint = waypoints.Dequeue();
+            lastKnownTargetPos = target.transform.position;
+        }
+    }
+}
