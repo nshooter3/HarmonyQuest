9e8dc4be67f0b7ebc4fe696f219489f9f6ed2738
nshooter3 <nick.shooter@gmail.com>
2019-11-06 23:15:22 -0500

    Pathfinding (#9)
    * moved nickzone to sandbox folder. created navmesh for h2 scene
    
    * first pass at basic pathfinding
    
    * Undid sandbox folder, as that should be done on a different branch. Changed the navmash script to a static util script, in order to decouple our agent traversal from the built in unity pathfinding.
    
    * first pass at custom navigation agent and generic enemy
    
    * first pass at enemy AI states. Enemy will now move directly towards player if there is navmesh line of sight, and generate a path to follow if there is not
    
    * enemy now retreats once agro is lost. condensed some redundant logic
    
    * cleaned up code, added summary tags to editor variables
    
    * Removed state class in favor of enums for enemy state management. It was making things unnecessarily complicated
    
    * Turned down enemy speed. Set enemy rigidbody interpolation mode to interpolate. Swapped magic number with variable.
    
    * enemy will now deaggro if there is no valid navmesh path to the player
    
    * Moved temp scripts out of the GameAI folder
    
    * added floating points to float vars defined as ints
    
    * Agents using the navmesh now raycast down from their transforms to the ground, using that point as their origin for calculating paths. This prevents airborne navigation agents from breaking, as they will now return to the ground before they start advancing towards the player if there is an obstacle in their way. Added a TraversableGround layer which is used by NavMeshUtil to identify the ground below agents. If an enemy is airborne in the scene, their origin will raycast to the ground to prevent errors when the enemy deaggros and tries to navigate to their origin.

---
+++ Assets/Scripts/GameAI/NavMeshUtil.cs
@@ -0,0 +1,75 @@
+﻿namespace GameAI
+{
+    using UnityEngine.AI;
+    using UnityEngine;
+
+    public static class NavMeshUtil
+    {
+        private static LayerMask traversableGroundLayerMask = LayerMask.GetMask("TraversableGround");
+
+        public static NavMeshPath GeneratePath(Transform source, Transform target, int areaMask = NavMesh.AllAreas)
+        {
+            return GeneratePath(source.position, target.position, NavMesh.AllAreas);
+        }
+
+        public static NavMeshPath GeneratePath(Vector3 source, Transform target, int areaMask = NavMesh.AllAreas)
+        {
+            return GeneratePath(source, target.position, NavMesh.AllAreas);
+        }
+
+        public static NavMeshPath GeneratePath(Transform source, Vector3 target, int areaMask = NavMesh.AllAreas)
+        {
+            return GeneratePath(source.position, target, NavMesh.AllAreas);
+        }
+
+        public static NavMeshPath GeneratePath(Vector3 source, Vector3 target, int areaMask = NavMesh.AllAreas)
+        {
+            NavMeshPath path = new NavMeshPath();
+            NavMesh.CalculatePath(source, target, areaMask, path);
+            return path;
+        }
+
+        public static float GetPathLength(NavMeshPath path)
+        {
+            if (path.status == NavMeshPathStatus.PathInvalid)
+            {
+                return -1.0f;
+            }
+
+            float length = 0.0f;
+            for (int i = 1; i < path.corners.Length; ++i)
+            {
+                length += Vector3.Distance(path.corners[i - 1], path.corners[i]);
+            }
+
+            return length;
+        }
+
+        public static bool IsNavMeshBelowAgent(Transform navigationAgent, out Vector3 raycastHitPosition)
+        {
+            RaycastHit raycastHit;
+            if (Physics.Raycast(navigationAgent.position, Vector3.down, out raycastHit, Mathf.Infinity, traversableGroundLayerMask))
+            {
+                raycastHitPosition = raycastHit.point;
+                return true;
+            }
+            else
+            {
+                raycastHitPosition = Vector3.zero;
+                return false;
+            }
+        }
+
+        public static bool IsTargetObstructed(Transform source, Transform target, int areaMask = NavMesh.AllAreas)
+        {
+            Vector3 startPos = source.position;
+            Vector3 raycastHit;
+            if (IsNavMeshBelowAgent(source, out raycastHit))
+            {
+                startPos = raycastHit;
+            }
+            NavMeshHit navmeshRaycastHit;
+            return NavMesh.Raycast(startPos, target.position, out navmeshRaycastHit, areaMask);
+        }
+    }
+}
