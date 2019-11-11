223f28131ec927451c5cd83c976e24b9871bfcfb
Matthew DeLong <matthewdelong@outlook.com>
2019-11-10 23:59:06 -0500

    Move new files to proper locations

---
+++ Assets/Scripts/CharacterControllers/Melody/MelodyController.cs
@@ -0,0 +1,42 @@
+﻿namespace Melody
+{
+    using HarmonyQuest;
+    using HarmonyQuest.Input;
+    using Melody.States;
+    using UnityEngine;
+
+    public class MelodyController : MonoBehaviour
+    {
+        readonly CharacterController characterController;
+        MelodyStateMachine StateMachine;
+
+        Vector3 move;
+
+        public float MaxSpeed = 5;
+
+        public IPlayerInputManager input { get; private set; }
+        public Animator animator { get; private set; }
+        public Rigidbody rigidBody { get; private set; }
+        public Vector3 Move { get => move; set => move = value; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rigidBody = gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
+            animator = gameObject.GetComponent(typeof(Animator)) as Animator;
+
+            input = ServiceLocator.instance.GetInputManager();
+
+            StateMachine = new MelodyStateMachine(this);
+
+            move = new Vector3();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            move.Set(input.GetHorizontalMovement(), 0, input.GetVerticalMovement());
+            StateMachine.OnUpdate(Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterControllers/Melody/MelodyStateMachine.cs
@@ -0,0 +1,28 @@
+﻿namespace Melody.States
+{
+    public class MelodyStateMachine
+    {
+        readonly MelodyController Controller;
+        MelodyState CurrentState;
+        MelodyState NextState;
+
+
+        public MelodyStateMachine(MelodyController Controller)
+        {
+            this.Controller = Controller;
+
+            CurrentState = new IdleState(Controller);
+        }
+
+        public void OnUpdate(float time)
+        {
+            CurrentState.OnUpdate(time);
+            if(CurrentState.CanExit() && CurrentState.NextState() != null)
+            {
+                NextState = CurrentState.NextState();
+                CurrentState = NextState;
+                NextState = null;
+            }
+        }
+    }
+}
