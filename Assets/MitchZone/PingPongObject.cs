7eb14126c386a033c9bc5895c5e6645f7b7e6f7a
Mitch McClellan <mitchelljmcclellan@gmail.com>
2019-12-03 02:02:23 -0500

    Updated test scene for demoing

---
+++ Assets/MitchZone/PingPongObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongObject : MonoBehaviour
+{
+    private Vector3 pos1 = new Vector3(0,-30,0);
+    private Vector3 pos2 = new Vector3(0,30,0);
+    public float speed = 1.0f;
+
+    void Update() {
+        transform.rotation = Quaternion.Lerp ( Quaternion.Euler(pos1), Quaternion.Euler(pos2), Mathf.PingPong(Time.time*speed, 1.0f));
+    }
+}
