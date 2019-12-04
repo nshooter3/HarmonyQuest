db7c05f9a5895d02b67c7975f5e72bc3afcdcd7f
nshooter3 <nick.shooter@gmail.com>
2019-12-03 20:05:08 -0500

    Changed AIAgentComponentInterface to AIGameObject and added interstitial enemy class

---
+++ Assets/Scripts/GameAI/GameObjects/Enemy.cs
@@ -0,0 +1,25 @@
+﻿namespace GameAI.AIGameObjects
+{
+    using GameAI.Navigation;
+    using GameAI.StateHandlers;
+
+    public abstract class Enemy : AIGameObject
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        /// <summary>
+        /// Implement this in the child class to specify what kind of state machine this agent will use.
+        /// </summary>
+        /// <returns> A new instance of this agent's state machine </returns>
+        public abstract override AIStateHandler GetStateHandler();
+
+        /// <summary>
+        /// Implement this in the child class to specify what kind of navigator this agent will use.
+        /// </summary>
+        /// <returns> A new instance of this agent's navigator </returns>
+        public abstract override Navigator GetNavigator();
+    }
+}
---
+++ Assets/Scripts/GameAI/GameObjects/FrogKnight.cs
@@ -0,0 +1,23 @@
+﻿namespace GameAI.AIGameObjects
+{
+    using GameAI.Navigation;
+    using GameAI.StateHandlers;
+
+    public class FrogKnight : Enemy
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override AIStateHandler GetStateHandler()
+        {
+            return new FrogKnightStateHandler();
+        }
+
+        public override Navigator GetNavigator()
+        {
+            return new GroundedNavmeshNavigator();
+        }
+    }
+}
