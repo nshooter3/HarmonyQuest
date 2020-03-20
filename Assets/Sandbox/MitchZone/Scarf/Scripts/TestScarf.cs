0418d15a5fdee3032c8a731ed0c5989352a8c54c
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-03-20 00:28:13 -0400

    Scarf (#17)
    * Created scarf test scene
    
    * Add attack anim and parry anim, and add jiggle physics to tube scarf
    
    * Added wavy secondary motion to the scarf
    
    * Added basic version of the dash
    
    * Added a stable scarf follow
    
    * Using Unity cloth for the scarf
    
    * Added scarf IK
    
    * Experimenting with cloth sim and IK together
    
    * Added working scarf bones following a low res scarf simulated cloth mesh
    
    * Added look at constraints to the bones so there is no weird looking rotation nonsense
    
    * Removed not working experiments
    
    * Moved Mitch folder into Sandbox
    
    * Added missing folder meta file
    
    * Undo whitespace changes to ServiceLocator.cs
    
    * Forgot the meta file for ServiceLocator

---
+++ Assets/Sandbox/MitchZone/Scarf/Scripts/TestScarf.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+// [ExecuteInEditMode ]
+public class TestScarf : MonoBehaviour
+{
+    public float dashProgress = 0f;
+    [SerializeField]
+    private TestPlayerScarf player;
+    [SerializeField]
+    private Transform ball;
+
+    private Animator scarfAnimator;
+
+
+
+    void Start()
+    {
+        scarfAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Shader.SetGlobalVector("_BallLocation", ball.transform.position);
+        Shader.SetGlobalVector("_PlayerLocation", player.transform.forward);
+        Shader.SetGlobalFloat("_Progress", dashProgress);
+        Quaternion rot = player.transform.rotation;
+        Quaternion rot2 = ball.transform.rotation;
+        Shader.SetGlobalVector("_PlayerRotation", new Vector4(rot.x, rot.y, rot.z, rot.w));
+        Shader.SetGlobalVector("_BallRotation", new Vector4(rot2.x, rot2.y, rot2.z, rot2.w));
+    }
+
+    public void Dash()
+    {
+        print("Scarf Dash");
+        scarfAnimator.SetTrigger("dash");
+    }
+}
