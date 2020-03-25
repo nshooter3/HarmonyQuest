8fcc872d68d9e061d91064e2264993778d183e0f
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-03-25 00:24:45 -0400

    Oil paint (#18)
    * Added working oil paint post processing effect
    
    * Added test fantasy scene and assets
    
    * Added Oil Paint post processing scripts and forest test scene
    
    * Added Anisotropic oil paint shader that compiles for the first time
    
    * Modified depth of the oil paint effect to be centered around a point
    
    * Updated test scene for demoing
    
    * Moved test scene
    
    * Oil paint focus area follows an object
    
    * Updated post processing script and added shader to graphics settings
    
    * Removing unused assets and other clean up
    
    * Removed uneeded code
    
    * Removing CameraControllerEditor.cs
    
    * Deleting unneeded scene

---
+++ Assets/Scripts/Effects/OilPaint/OilPaintPostProcessing.cs
@@ -0,0 +1,81 @@
+﻿namespace HarmonyQuest.Effects.OilPaint
+{
+    using UnityEngine;
+    using UnityEngine.Rendering.PostProcessing;
+
+    [System.Serializable]
+    [PostProcess(typeof(OilPaintPostProcessingRenderer), PostProcessEvent.AfterStack, "Harmony Quest/Oil Paint")]
+    public class OilPaintPostProcessing : PostProcessEffectSettings
+    {
+        public enum Target { ViewSpace, WorldSpace }
+
+        [System.Serializable]
+        public sealed class TargetParameter : ParameterOverride<Target> {}
+        [System.Serializable]
+        public sealed class TransformParameter : ParameterOverride<Transform> { }
+
+        public TargetParameter target = new TargetParameter();
+        [Range(1f, 9f)]
+        public FloatParameter radius = new FloatParameter() { value = 5.0f };
+        [Range(0f, 15f)]
+        public FloatParameter distance = new FloatParameter() { value = 1.0f };
+        [Range(0f, 1f)]
+        public FloatParameter thickness = new FloatParameter() { value = 0.5f };
+        // public TransformParameter point = new TransformParameter() { };
+    }
+
+    public class OilPaintPostProcessingRenderer<T> : PostProcessEffectRenderer<T> where T : OilPaintPostProcessing
+    {
+        private int _RadiusID, _DistanceID, _ThicknessID, _InverseViewID, _PointID;
+
+        private Shader shader;
+
+        private Transform point;
+
+        public override void Init()
+        {
+            base.Init();
+            shader = Shader.Find("Hidden/OilPaintPostProcess");
+            _RadiusID = Shader.PropertyToID("_Radius");
+            _DistanceID = Shader.PropertyToID("_Distance");
+            _ThicknessID = Shader.PropertyToID("_Thickness");
+            _InverseViewID = Shader.PropertyToID("_InverseView");
+            _PointID = Shader.PropertyToID("_Point");
+
+            // if (settings.point == null)
+            // {
+            //     settings.point = new OilPaintPostProcessing.TransformParameter() { value =  };
+            // }
+            point = GameObject.FindObjectOfType<OilPaintPoint>().transform;
+        }
+
+        public override void Render(PostProcessRenderContext context)
+        {
+            var cmd = context.command;
+            cmd.BeginSample("Oil Paint");
+            var sheet = context.propertySheets.Get(shader);
+
+            sheet.properties.SetFloat(_RadiusID, settings.radius);
+            sheet.properties.SetFloat(_DistanceID, settings.distance);
+            sheet.properties.SetFloat(_ThicknessID, settings.thickness);
+            sheet.properties.SetMatrix(_InverseViewID, context.camera.cameraToWorldMatrix);
+            sheet.properties.SetVector(_PointID, point.position);
+
+            var pass = (int)settings.target.value;
+            cmd.BlitFullscreenTriangle(context.source, context.destination, sheet, pass);
+
+            cmd.EndSample("Oil Paint");
+        }
+
+        public override DepthTextureMode GetCameraFlags()
+        {
+            // if (settings == null)
+            // return DepthTextureMode.None;
+
+            return DepthTextureMode.Depth;
+            // return base.GetCameraFlags();
+        }
+    }
+
+    public sealed class OilPaintPostProcessingRenderer : OilPaintPostProcessingRenderer<OilPaintPostProcessing> { }
+}
