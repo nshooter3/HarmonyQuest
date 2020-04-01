4286b137588ac8d9726579cc01a1d895009eacd3
nshooter3 <nick.shooter@gmail.com>
2020-04-01 04:04:24 -0400

    First pass at handling multiple lock on targets

--- Assets/Scripts/CharacterControllers/Melody/MelodyLockOn.cs
+++ Assets/Scripts/CharacterControllers/Melody/MelodyLockOn.cs
@@ -39,13 +39,27 @@
 
         public void GetNearestLockonTarget()
         {
+            float closestAngle = int.MaxValue;
+            float curAngle;
             potentialLockOnTargets = aiAgentManager.GetLivingAgents();
             for (int i = 0; i < potentialLockOnTargets.Count; i++) {
-                lockonTarget = potentialLockOnTargets[i];
-                curTargetIndex = i;
+                curAngle = GetTargetAngle(potentialLockOnTargets[i].aiGameObject.transform.position);
+                if (curAngle < closestAngle)
+                {
+                    closestAngle = curAngle;
+                    lockonTarget = potentialLockOnTargets[i];
+                    curTargetIndex = i;
+                }
             }
             lockOnReticule.SetTarget(lockonTarget.aiGameObject.transform);
             lockOnImage.enabled = true;
+        }
+
+        public float GetTargetAngle(Vector3 targetPos)
+        {
+            //Calculate the angle of the target by getting the angle between the target position relative to the player, and the direction the player is facing.
+            Vector3 sourceDirection = targetPos - controller.transform.position;
+            return Vector3.Angle(controller.transform.forward, sourceDirection);
         }
 
         public void ChangeLockonTargetRightStick(int xAxis, int yAxis)
