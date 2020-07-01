722516a7ee017f5be545e773a5bc6c94e32326ff
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-07-01 12:05:45 -0400

    Revert "Swamp scene (#27)"
    This reverts commit 038a534d6c0fdc24e17c5aeb3de9a04dfdbc6971.

--- Assets/Scripts/CharacterControllers/Melody/MelodyStats.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Melody
-{
-    public static class MelodyStats
-    {
-        public static int maxHealth = 100;
-        public static int attackDamage = 10;
-        public static int counterDamage = 40;
-    }
-}
