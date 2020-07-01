0f5ae1bb492e980f352053493350ce7f355ca7b1
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-07-01 12:06:17 -0400

    Revert "Swamp scene (#27)" (#31)
    This reverts commit 038a534d6c0fdc24e17c5aeb3de9a04dfdbc6971.

--- Assets/Scripts/Effects/OilPaint/OilPaintPoint.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OilPaintPoint : MonoBehaviour
-{
-    // Processing effect searches for game object with this script attached to determine center point of the effect
-}
